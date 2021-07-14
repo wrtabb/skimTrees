13e96324ae2e1bf393c25f9f3bf0f395120c2ea1
Robert Tabb <wrtabb@gmail.com>
2021-07-14 16:05:17 -0500

    updated the process of combining skims with HADD

---
+++ haddFiles/processHadd.C
@@ -0,0 +1,13 @@
+TString base_directory = "/mnt/hadoop/user/uscms01/pnfs/unl.edu/data4/cms/store/user/wtabb/DrellYan_13TeV_2016/v2p6/";	
+
+void processHadd(TString sample,TString output_file)
+{
+	TString file_location = base_directory;	
+	file_location += sample;
+	file_location += "/skims_0001/*.root";
+	TString hadd_execute = "hadd -f ";
+	hadd_execute += output_file;
+	hadd_execute += ".root ";
+	hadd_execute += file_location;
+	gSystem->Exec(hadd_execute);	
+}
